ff7b8d8be1bd69405394835ae4cf9af82a5e2605
MuhammadRasyidF <rasyid201101@gmail.com>
2022-09-05 01:39:27 +0700

    75%

--- mission2_VanillaJS/Assets/animate.js
+++ mission2_VanillaJS/Assets/animate.js
@@ -6,7 +6,7 @@ function moveShip() {
     let timer = setInterval(() => {
       ship.style.left = move + "px";
       move = move + 1 * changeDir;
-      if (move >= 200) {
+      if (move >= 150) {
         changeDir = -1;
       } else if (move <= 100) {
         changeDir = 1;
@@ -15,20 +15,22 @@ function moveShip() {
   });
 }
 
-function moveShip2() {
+function appearBG() {
   window.addEventListener("load", (event) => {
-    var animate;
-    var move = 100;
-    var changeDir = 1;
-    let timer = setInterval(() => {
-      ship2.style.left = move + "px";
-      move = move + 1 * changeDir;
-      if (move >= 200) {
-        changeDir = -1;
-      } else if (move <= 100) {
-        changeDir = 1;
-      }
-    }, 20);
+    let distance = -200;
+    let moveIn = setInterval(() => {
+      stones.style.bottom = distance + "px";
+      moon.style.top = distance + "px";
+      distance += 1;
+      if (distance == 0) clearInterval(moveIn);
+    }, 5);
+
+    let opacity = 0;
+    let fadeIn = setInterval(() => {
+      opacity += 0.05;
+      command.style.opacity = opacity;
+      if (opacity == 1) clearInterval(fadeIn);
+    }, 50);
   });
 }
 
@@ -38,5 +40,5 @@ function moveShip2() {
 //   time = 0;
 // }
 
+appearBG();
 moveShip();
-moveShip2();
