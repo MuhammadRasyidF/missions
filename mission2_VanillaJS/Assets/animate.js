b495968b6ccf91522592225198725979ea16e9c9
MuhammadRasyidF <rasyid201101@gmail.com>
2022-09-05 11:57:08 +0700

    85%

--- mission2_VanillaJS/Assets/animate.js
+++ mission2_VanillaJS/Assets/animate.js
@@ -1,15 +1,75 @@
 function moveShip() {
   window.addEventListener("load", (event) => {
-    var animate;
     var move = 100;
+    var updown = 270;
     var changeDir = 1;
-    let timer = setInterval(() => {
+    var changeDirUp = 1;
+    setInterval(() => {
       ship.style.left = move + "px";
-      move = move + 1 * changeDir;
-      if (move >= 150) {
+      ship.style.top = updown + "px";
+
+      move = move + 0.3 * changeDir;
+      updown = updown + 0.2 * changeDirUp;
+
+      if (move >= 120) {
         changeDir = -1;
       } else if (move <= 100) {
         changeDir = 1;
+      }
+      if (updown >= 280) {
+        changeDirUp = -1;
+      } else if (updown <= 270) {
+        changeDirUp = 1;
+      }
+    }, 20);
+  });
+}
+
+function moveKinton() {
+  window.addEventListener("load", (event) => {
+    var move = 60;
+    var move2 = 30;
+    var move3 = 45;
+    var move4 = 80;
+    var move5 = 5;
+    var move6 = 90;
+    var Dir = -1;
+    setInterval(() => {
+      cloud1.style.left = move + "vw";
+      cloud2.style.left = move2 + "vw";
+      cloud3.style.left = move3 + "vw";
+      cloud4.style.left = move4 + "vw";
+      cloud5.style.left = move5 + "vw";
+      cloud6.style.left = move6 + "vw";
+      move = move + 0.2 * Dir;
+      move2 = move2 + 0.225 * Dir;
+      move3 = move3 + 0.217 * Dir;
+      move4 = move4 + 0.22 * Dir;
+      move5 = move5 + 0.2 * Dir;
+      move6 = move6 + 0.215 * Dir;
+      if (move <= -10) {
+        cloud1.style.left = 100 + "vw";
+        move = 100;
+      }
+      if (move2 <= -10) {
+        cloud2.style.left = 100 + "vw";
+        move2 = 100;
+      }
+      if (move3 <= -10) {
+        cloud3.style.left = 100 + "vw";
+        move3 = 100;
+      }
+      if (move4 <= -10) {
+        cloud4.style.left = 100 + "vw";
+        move4 = 100;
+      }
+      if (move5 <= -10) {
+        cloud5.style.left = 100 + "vw";
+        move5 = 100;
+      }
+      if (move6 <= -10) {
+        cloud6.style.left = 100 + "vw";
+        move6 = 100;
       }
     }, 20);
   });
@@ -42,3 +102,4 @@ function appearBG() {
 
 appearBG();
 moveShip();
+moveKinton();
